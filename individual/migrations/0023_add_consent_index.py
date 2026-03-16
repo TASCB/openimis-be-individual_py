fa91a7c5ff60768253a8d2ba9d25ee9f3e8e00eb
Jeffharan <haran.jeff@gmail.com>
2026-03-16 13:35:26 +0300

    Fix migration dependency

--- individual/migrations/0023_add_consent_index.py
+++ individual/migrations/0023_add_consent_index.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('individual', '0018_alter_groupindividual_role_and_more'),
+        ('individual', '0022_pmtrunprogress'),
     ]
 
     operations = [
